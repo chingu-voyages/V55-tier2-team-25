ca3768e3d565d7411024d82f978543095d2b8b99
Alison <alison@andersonholland.com>
2025-05-15 13:10:10 -0500

    Changed Footer class, accidentally changed in last commit

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <header className="p-p border-2">
+      <header className="p-4 border-2">
         <h2 className="text-2xl font-bold">My Footer</h2>
         {/* We can code our information here */}
       </header>
