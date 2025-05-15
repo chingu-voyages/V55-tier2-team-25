257509b3e9788410ca54757b15369ec5adfb7c87
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-05-14 23:40:42 -0400

    add filter icon and React Icons dependency, fixed Tailwind connection

--- react-app/src/components/ClearButton.jsx
+++ react-app/src/components/ClearButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ClearButton() {
   return (
     <div>
-      <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">
+      <button className="bg-gray-500 text-white p-2 rounded hover:bg-gray-700">
         Clear Search
       </button>
     </div>
