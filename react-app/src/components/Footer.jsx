48f0ec9ee7160b8f2e9ef76ef9b6ed285419ae1a
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-05-16 15:49:38 -0400

    mobile styling for footer

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 export default function Footer() {
   return (
     //this is the component below div
-    <div className="flex justify-center">
-      <header className="p-4 border-2">
-        <h2 className="text-2xl font-bold text-center">Team Members</h2>
-        <ul className="pt-4 pb-4">
+    <div className="p-4">
+      <header className="border-2 whitespace-normal break-words">
+        <h2 className="text-2xl font-bold text-center pt-4">Team Members</h2>
+        <ul className="p-4">
           <li>
             Marissa Lamothe: [GitHub](https://github.com/msrissaxox) /
             [LinkedIn](https://linkedin.com/in/marissalamothe/)
