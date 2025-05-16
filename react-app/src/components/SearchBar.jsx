1c9275d7bc36391874dbee5b2fac821289d7d027
val_l <contacttovalerie@gmail.com>
2025-05-15 22:44:01 -0400

    Worked on search bar styling

--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { FiFilter } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
 
 export default function SearchBar() {
   return (
-    <div>
-      <FiFilter className="m-5"/>
+    <div className="flex flex-row w-full items-center justify-center">
+      <FiFilter className="p-5 m-3 w-[10%] bg-[#E2E2E2] rounded"/>
 
       <label htmlFor="search-bar" className="sr-only">
         Search Bar
       </label>
-      <input
-        type="text"
-        id="search-bar"
-        name="search-bar"
-        placeholder="Search..."
-        className="border rounded p-2"
-      />
+
+
+        <FiSearch className=" "/>
+        <input
+          type="text"
+          id="search-bar"
+          name="search-bar"
+          placeholder="
+          Pick a topic..."
+          className="border rounded p-2 w-[75%]"
+        />
+
     </div>
   );
 }
