46a596039c5f47a37576a87170d2ad247c1867cf
Alison <alison@andersonholland.com>
2025-05-27 15:24:51 -0500

    Added pointer to reset button

--- react-app/src/components/ClearButton.jsx
+++ react-app/src/components/ClearButton.jsx
@@ -4,7 +4,7 @@ const ClearButton = ({onClick}) => {
   // This component renders a button that clears the search input
   return (
     <div>
-      <button className=""
+      <button className="cursor-pointer"
                    onClick={onClick}>
         Reset
       </button>
