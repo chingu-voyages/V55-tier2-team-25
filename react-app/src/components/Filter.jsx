7a148216e54544b52890c4bdb253df01398ed686
Alison <alison@andersonholland.com>
2025-05-30 10:36:06 -0500

    Added change in filter button color with menu open

--- react-app/src/components/Filter.jsx
+++ react-app/src/components/Filter.jsx
@@ -58,7 +58,9 @@ export default function Filter({
   }
 
   return (
-    <div className="absolute left-1.5 -top-25"> {/* positioning of filter icon */}
+    <div className="absolute left-1.5 -top-25">
+      {" "}
+      {/* positioning of filter icon */}
       <button
         type="button"
         onClick={toggleMenu}
@@ -66,62 +68,67 @@ export default function Filter({
         aria-controls="filter-menu"
         className="cursor-pointer flex items-center"
       >
-        <FiFilter className="text-5xl text-white bg-gradient-to-r from-[var(--gradient-1)] to-[var(--gradient-2)] rounded-full p-2" />
+        <FiFilter
+          className={`text-5xl rounded-full p-2 ${
+            isOpen
+              ? "text-[var(--gradient-1)] bg-white border-2 border-[var(--gradient-1)]"
+              : "text-white bg-gradient-to-r from-[var(--gradient-1)] to-[var(--gradient-2)]"
+          }`}
+        />
         <span className="sr-only">Filter menu based on resource tags</span>
       </button>
       <div className="relative">
+        <div
+          id="filter-menu"
+          role="dialog"
+          aria-label="Filter menu"
+          aria-hidden="true"
+          tabIndex={-1}
+          className={`absolute left-0 top-full mt-3 w-[93vw] p-4 bg-white border border-gray-200 z-10 rounded-2xl shadow-lg ${isOpen ? "block" : "hidden"}`}
+        >
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold text-lg">Filters</h2>
+            <ClearButton onClick={handleClearSearch} name="Reset" />
+          </div>
 
-      <div
-        id="filter-menu"
-        role="dialog"
-        aria-label="Filter menu"
-        aria-hidden="true"
-        tabIndex={-1}
-        className={`absolute left-0 top-full mt-3 w-[93vw] p-4 bg-white border border-gray-200 z-10 rounded-2xl shadow-lg ${isOpen ? "block" : "hidden"}`}
-      >
-        <div className="flex justify-between items-center">
-          <h2 className="font-bold text-lg">Filters</h2>
-          <ClearButton onClick={handleClearSearch} name="Reset" />
+          <fieldset className="mt-4">
+            <legend className="pl-3 flex flex-wrap gap-1 pt-4">
+              <FiTag className="self-center" />
+              <span className="sr-only">Filter by</span>Tags
+            </legend>
+            <ul className="flex flex-wrap gap-1">
+              {tags.map((tag) => (
+                <li key={tag.id}>
+                  <label
+                    htmlFor={`tag-${tag.id}`}
+                    className="flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors"
+                  >
+                    <input
+                      type="checkbox"
+                      id={`tag-${tag.id}`}
+                      value={tag.tag}
+                      checked={(selectedTags || []).includes(tag.tag)}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        setSelectedTags(
+                          (prev) =>
+                            prev.includes(value)
+                              ? prev.filter((tag) => tag !== value) // remove if already selected
+                              : [...prev, value] // add if not selected
+                        );
+                      }}
+                      className="peer hidden" // gives it a button look rather than a checkbox list
+                    />
+                    <span className="inline-block px-3 py-3 rounded-full border border-[var(--foreground)] bg-white text-[var(--foreground)] peer-checked:border-[var(--gradient-1)] peer-checked:text-[var(--gradient-1)] transition-all">
+                      {tag.tag}
+                    </span>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
         </div>
-
-        <fieldset className="mt-4">
-          <legend className="pl-3 flex flex-wrap gap-1 pt-4">
-            <FiTag className="self-center" />
-            <span className="sr-only">Filter by</span>Tags
-          </legend>
-          <ul className="flex flex-wrap gap-1">
-            {tags.map((tag) => (
-              <li key={tag.id}>
-                <label
-                  htmlFor={`tag-${tag.id}`}
-                  className="flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors"
-                >
-                  <input
-                    type="checkbox"
-                    id={`tag-${tag.id}`}
-                    value={tag.tag}
-                    checked={(selectedTags || []).includes(tag.tag)}
-                    onChange={(e) => {
-                      const value = e.target.value;
-                      setSelectedTags(
-                        (prev) =>
-                          prev.includes(value)
-                            ? prev.filter((tag) => tag !== value) // remove if already selected
-                            : [...prev, value] // add if not selected
-                      );
-                    }}
-                    className="peer hidden" // gives it a button look rather than a checkbox list
-                  />
-                  <span className="inline-block px-3 py-3 rounded-full border border-[var(--foreground)] bg-white text-[var(--foreground)] peer-checked:border-[var(--gradient-1)] peer-checked:text-[var(--gradient-1)] transition-all">
-                    {tag.tag}
-                  </span>
-                </label>
-              </li>
-            ))}
-          </ul>
-        </fieldset>
       </div>
-    </div>
     </div>
   );
 }
