e5c034f8c693a99ca874ac23f6e23f6ffb57d57b
val_l <contacttovalerie@gmail.com>
2025-06-01 01:58:55 -0400

    Fixed footer bug. Added gradient

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -1,14 +1,13 @@
 //This is the header component
 
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-<<<<<<wip-footer-valerie
     <div>
         {/* */}
-      <div className="team-info flex justify-center flex-col items-center">   {/*flex justify center flex-direction column #385894 #473CE0 #682FD8*/}
-        <div className="footer-bold flex justify-between w-full">{/* flex justify-space apart*/}
+      <div className="team-info flex justify-center flex-col items-center bg-[linear-gradient(to_right,_#385894_0%,_#473CE0_48%,_#682FD8_85%)]">   {/*flex justify center flex-direction column #385894 #473CE0 #682FD8*/}
+        <div className="footer-bold flex justify-around w-full">{/* flex justify-space around*/}
           <h3>Team</h3>
           <h3>icon</h3>
           {/* Github Icon*/}
@@ -28,21 +27,67 @@ export default function Footer() {
 
       </div>
 
+      <div className="copyright bg-green-500 text-center"> {/*bring up opacity*/}
+        <p>@2025 Chingu. All rights reserved.</p>
+      </div>
+  </div>
+  );
+}
+
+
+
+    
+
+{/*export default function Footer() {
+  return (
+    <div>
+       
+      <div className="team-info flex justify-center flex-col items-center">   {/*flex justify center flex-direction column #385894 #473CE0 #682FD8*/}
+       {/* <div className="footer-bold flex justify-between w-full"> flex justify-space apart*
+          <h3>Team</h3>
+          <h3>icon</h3>
+          {/* Github Icon*
+        </div>
+
+        <div className="name-container flex w-4/5">  {/* flex flex-direction row width 80%?
+          <p className="w-3/10 text-left">Alison Holland</p>{/*width 30% align-text left 
+          <p className="w-3/10 text-left">Jim Schibler</p>
+          <p className="w-3/10 text-left">Marissa Lamothe</p>
+        </div>
+
+        <div className="name-container flex w-4/5">
+          <p className="w-3/10 text-left">Myoungjin Lee</p>
+          <p className="w-3/10 text-left">Molly</p>
+          <p className="w-3/10 text-left">Valerie</p>
+        </div>
+
+      </div>
+
       <div className="copyright">
         <p>@2025 Chingu. All rights reserved.</p>
       </div>
 
 
-      {/*<header className="p-4 border-2">
+      </div><header className="p-4 border-2">
         <h2 className="text-2xl font-bold">My Footer</h2>
          We can code our information here 
-      </header>*/}
-=======
-  
-    //this is the component below div
-    <div className="p-4">
+      </header>
+    </div>  
+
+  );
+
+
+  /*  <div className="p-4">
       <header className="border-2 whitespace-normal break-words">
         <h2 className="text-2xl font-bold text-center pt-4">Team Members</h2>
+
+      </header>
+
+    </div>
+
+
+
+
         <ul className="p-4">
           <li>
             Marissa Lamothe: 
@@ -78,9 +123,4 @@ export default function Footer() {
             James: 
             <a href="https://github.com/Nwumfor" target='_blank'>GitHub</a> / <a href="https://www.linkedin.com/in/james-nwumfor-56008144/" target='_blank'>LinkedIn</a>
           </li>
-        </ul>
-      </header>
->>>>> main
-    </div>
-  )
-}
+        </ul>*/}
