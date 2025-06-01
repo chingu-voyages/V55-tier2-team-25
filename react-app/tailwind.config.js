512b4d25c36532853caf0f57745fc8d71dcdd1fb
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-06-01 14:07:12 -0400

    manually added tailwind.config.js and added import for pretendard font

---
+++ react-app/tailwind.config.js
@@ -0,0 +1,17 @@
+/**manually added this file */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          pretendard: ['Pretendard', 'sans-serif'],
+        }
+      },
+    },
+    plugins: [],
+  }
+  
