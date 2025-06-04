f075407a0a3d12c7495f005c10b6ac5ddb18c3ac
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-06-04 15:15:45 -0400

    height change for PeerPick image- added max height, padding changes for h3s, font changes for h3s, padding changes for header

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -8,14 +8,15 @@ export default function Header() {
 
   return (
     <div>
-      <header className="flex flex-col bg-gradient-to-r from-[#385894] via-[#473CE0] to-[#682FD8] rounded-b-4xl p-10 pb-28">
+      <header className="flex flex-col bg-gradient-to-r from-[#385894] via-[#473CE0] to-[#682FD8] rounded-b-4xl p-10 pb-28 md:p-20 md:pb-30 lg:p-25 lg:pb-30">
+        {/*pb-28*/}
         <div className="flex justify-between w-full text-white">
           <div className="flex w-1/2">
             <h1>
               <img
                 src="peerpicks-logo-white.png"
                 alt="PeerPicks"
-                className="h-auto object-contain"
+                className="max-h-10 min-h-4 h-auto object-contain"
               />
             </h1>
             {/*<h1 className="text-2xl font-bold md:text-3xl">Peer Picks</h1>-->*/}
@@ -24,11 +25,13 @@ export default function Header() {
             {getCurrentDate()}
           </h2>
         </div>
-        <div className="flex flex-col self-start headerText text-white font-bold text-xl pt-[1.5em] text-left font-pretendard">
+        <div className="flex flex-col self-start headerText text-white font-bold text-xl md:text-2xl lg:text-3xl pt-[1.5em] text-left font-pretendard md:pt-12 md:pb-8 lg:pt-16 lg:pb-12">
           <h3>
-            Find your next <br />
-            PeerPick <br />
-            try tags + Keywords
+            Find Your Next Pick.
+            <br />
+            Using Tags + Keywords.
+            <br />
+            Explore Now.
           </h3>
         </div>
       </header>
