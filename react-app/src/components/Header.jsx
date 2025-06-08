493afa2910dac723f000683b483d585ab329bfdf
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-06-07 20:36:31 -0400

    changes to styling for desktop header

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { selectIsMobile } from "@/redux/dataSlice";
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { selectIsMobile } from '@/redux/dataSlice'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
-  const isMobile = useSelector(selectIsMobile);
+  const isMobile = useSelector(selectIsMobile)
 
   function getCurrentDate() {
     const date = new Date()
@@ -12,15 +12,15 @@ export default function Header() {
 
   return (
     <div>
-      <header className="flex flex-col bg-gradient-to-r from-[#385894] via-[#473CE0] to-[#682FD8] rounded-b-4xl p-10 pb-28 md:p-20 md:pb-30 lg:p-25 lg:pb-30">
+      <header className="flex flex-col lg:items-center bg-gradient-to-r from-[#385894] via-[#473CE0] to-[#682FD8] rounded-b-4xl p-10 pb-28 md:p-20 md:pb-30 lg:pt-16 lg:pb-30">
         {/*pb-28*/}
-        <div className="flex justify-between w-full text-white">
+        <div className="flex justify-between w-full text-white lg:max-w-[1200px]">
           <div className="flex w-1/2">
             <h1>
               <img
                 src="peerpicks-logo-white.png"
                 alt="PeerPicks"
-                className="max-h-10 min-h-4 h-auto object-contain"
+                className="max-h-10 lg:max-h-5 min-h-4 h-auto object-contain"
               />
             </h1>
             {/*<h1 className="text-2xl font-bold md:text-3xl">Peer Picks</h1>-->*/}
@@ -29,7 +29,7 @@ export default function Header() {
             {getCurrentDate()}
           </h2>
         </div>
-        <div className="flex flex-col self-start headerText text-white font-bold text-xl md:text-2xl lg:text-3xl pt-[1.5em] text-left font-pretendard md:pt-12 md:pb-8 lg:pt-16 lg:pb-12">
+        <div className="flex flex-col self-start lg:self-center headerText text-white font-bold text-xl md:text-2xl lg:text-5xl lg:text-center lg:text-3xl lg:pt-6 lg:pb-6 pt-[1.5em] text-left font-pretendard md:pt-12 md:pb-8">
           <h3>
             Find Your Next Pick.
             <br />
